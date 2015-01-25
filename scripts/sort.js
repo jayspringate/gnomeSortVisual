5107cac7de45cd9d0c880e5b9abd522acd31bcb8
jayspringate <jay.springate@gmail.com>
2015-01-24 19:34:18 -0800

    adding functioning gnome sort page with styling, and skeleton of home page

--- scripts/sort.js
+++ scripts/sort.js
@@ -1,35 +1,26 @@
-var winList = [["Kentucky", .763],
-							 ["Kansas", .722],
-							 ["North Carolina", .737],
-							 ["Duke", .706], 
-							 ["Syracuse", .694], 
-							 ["Temple", .643], 
-							 ["UCLA", .693], 
-							 ["St Johns", .653],
-							 ["Notre Dame", .646],
-							 ["Indiana", .639]];
-
 $(document).ready(function() {
-	$("#two").insertBefore("#one");
-
-function gnomeSort() {
-	var position = 0;
-	var temp = 0;
-	while (position < winList.length) {
-		if (position == 0 || winList[position - 1][1] <= winList[position][1]) {
-			position++;
-		}	else {
-			temp = winList[position - 1];
-			tempPositionString = (position - 1).toString;
-			winList[position - 1] = winList[position];
-			winList[position] = temp;
-			position--;
-		}
-	}
-}
+	
+	$(".button").on("click", function () {
+		var position = 0;
+		
+		function gnomeSort() {
+			if (position < $(".team").length) {
+				setTimeout(gnomeSort, 250);
+				$($(".team")[position]).addClass("highlight");
+				if (position == 0 || $($(".team")[position - 1]).attr("class") <= $($(".team")[position]).attr("class")) {
+					position++;
+					$($(".team")[position]).addClass("highlight");
+					$($(".team")[position - 1]).removeClass("highlight");
+			} else {
+				$($(".team")[position]).insertBefore($($(".team")[position - 1]));
+				position--;
+				$($(".team")[position + 1]).removeClass("highlight");
+			}
+		};
+	};
+	gnomeSort();
+	});
 });
 
-gnomeSort();
 
-console.log(winList);
 
